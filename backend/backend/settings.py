a37dc35e325e7b2d1533e651da966382e1cbf259
Judy <judy.zhu@learnosity.com>
2019-12-11 18:40:03 +1100

    [BUG] Fixed the  SystemCheckError
    ?: (corsheaders.E013) Origin '/' in CORS_ORIGIN_WHITELIST is missing  scheme or netloc
    	HINT: Add a scheme (e.g. https://) or netloc (e.g. example.com).

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = '/static/'
 
 # we whitelist localhost:3000 because that's where frontend will be served
 CORS_ORIGIN_WHITELIST = (
-     'localhost:3000/'
+     'http://localhost:3000',
  )
